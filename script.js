d6e8da147ae2d75f6d7e713bb3a41ce626b09bd7
Ziyodillo <100797218+ZiyodilloAnduqayumov@users.noreply.github.com>
2022-03-03 16:54:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// console.log(5+5);
+// console.log(5-5);
+// console.log(5*5);
+// console.log(5/5);
+
+// const x=19, y=31;
+
+// console.log(x+y);
+
+// prompt("Ismingizni kiriting ");
+
+
+// console.log("Hello "+" Bob");
+// const num = 5 + "hello";
+// console.log(5+"salom");
+
+
+
+
